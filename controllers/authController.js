e88c22116781c0e10b62090a1ca1a50e1b2db9fb
ramkumar gurav <116423006+RamkumarGurav@users.noreply.github.com>
2023-05-20 09:54:49 +0530

    made cookie's options "secure" to true

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ), //browser will delete this cookie after 90 days
-    secure: false, //if secure is true then this cookie will be sent through only https connection (encrpted connect)
+    secure: true, //if secure is true then this cookie will be sent through only https connection (encrpted connect)
     httpOnly: true, //this makes- this cookie can't be access or modified by the browser.or even browser cant delete this cookie//this makes the browser only store the cookie and send it along request every time a request made to the website server(where cookie originally created )
   };
 
