32738f458ca3244295a55f7858d88fd691dd27ec
RamkumarGurav <ramkumar.1js15ec071@gmail.com>
2023-05-22 19:45:24 +0530

    update blogRoute

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ const app = express();
 //---------------------xxx-----------------------------------
 
 //
-app.options("/blogs", cors());
 
 const corsOptions = {
   credentials: true, //all the credentials like cookies ,sessions are allowed
-  origin: true, // for public api //all the domains are allowed to call our api
-  // origin: ["https://my-exams-ramkumargurav.vercel.app","https://snextjs-h3ruppdy0-ramkumargurav.vercel.app","http://localhost:3000"], // Add your frontend origin here (Don't add '/' at the end).
+  // origin: true, // for public api //all the domains are allowed to call our api
+  origin: [
+    "https://my-exams-ramkumargurav.vercel.app",
+    "https://snextjs-h3ruppdy0-ramkumargurav.vercel.app",
+    "http://localhost:3000",
+  ], // Add your frontend origin here (Don't add '/' at the end).
   methods: ["GET", "PATCH", "DELETE", "POST", "PUT", "HEAD", "OPTIONS"], //methods that are allowed in cors
   allowedHeaders: [
     //this headers are allowed
@@ -53,6 +56,7 @@ const corsOptions = {
   ],
 };
 app.use("*", cors(corsOptions)); // npm i cors
+app.options("*", cors(corsOptions)); // enabling preflight call
 //--------------------------------------------------------
 
 app.use(cookieParser()); // To parse the incoming cookies
