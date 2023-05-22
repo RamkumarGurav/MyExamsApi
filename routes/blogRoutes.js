a8747d5fc6c5f8c4a811ff1fffdd04630d6da306
RamkumarGurav <ramkumar.1js15ec071@gmail.com>
2023-05-22 10:43:50 +0530

    update blogRoute

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const authController = require("../controllers/authController");
 const blogController = require("../controllers/blogController");
 const router = express.Router();
 
-
 // GET ALL USERS
 router.route("/blogs").get(blogController.getAllBlogs);
-router.route("/blogs").post(blogController.createBlog);
+router
+  .route("/blogs")
+  .post(authController.isRouteProtected, blogController.createBlog);
 
 // UPDATE AND DELETE Blog
 router
   .route("/blogs/:blogId")
   .get(blogController.getBlog)
   .patch(blogController.updateBlog)
-  .delete(blogController.deleteBlog);
+  .delete(authController.isRouteProtected, blogController.deleteBlog);
 
 module.exports = router;
