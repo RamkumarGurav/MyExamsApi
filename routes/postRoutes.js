083a9025d6bdeff590450380c35358df401556f0
RamkumarGurav <ramkumar.1js15ec071@gmail.com>
2023-05-23 18:28:43 +0530

    updated blog

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const postController = require("../controllers/postController");
+const blogController = require("../controllers/blogController");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
 // Login and Registation and LogoutRoutes
-router.get("/posts", postController.getAllPosts);
+router.get("/posts", blogController.getAllMyBlogs);
 router.post("/posts", postController.createPost);
 
 module.exports = router;
