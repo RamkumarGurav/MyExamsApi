130fe9c77a41fd60d60aeda43474cd34d8a13f83
RamkumarGurav <ramkumar.1js15ec071@gmail.com>
2023-05-24 12:05:20 +0530

    updated favicon

--- app.js
+++ app.js
@@ -27,7 +27,12 @@ const app = express();
 
 const corsOptions = {
   credentials: true, //all the credentials like cookies ,sessions are allowed
-  origin: true, // for public api //all the domains are allowed to call our api
+  // origin: true, // for public api //all the domains are allowed to call our api//
+  origin: [
+    "https://my-exams-ramkumargurav.vercel.app",
+    "https://myblogs-ramkumargurav.vercel.app",
+    "http://localhost:3000",
+  ],
   methods: "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE",
   allowedHeaders:
     "Access-Control-Allow-Headers, Origin,Accept,Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
