adc14b3dfdf704a068535e45115792e809dba227
RamkumarGurav <ramkumar.1js15ec071@gmail.com>
2023-05-29 16:57:39 +0530

    adding vilaPackageBooking Model

--- routes/villaPackageBookingRoutes.js
+++ routes/villaPackageBookingRoutes.js
@@ -4,17 +4,29 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router
+  .route("/villa-package-bookings")
+  .get(
+    authController.isRouteProtected,
+    authController.restrictTo("admin"),
+    villaPackageBookingController.getAllVillaPackageBookings
+  );
+
+router
   .route("/villa-package-bookings/create-checkout-session")
   .post(villaPackageBookingController.getCheckoutSession);
 
 router
-  .route("/villa-package-bookings")
-  .get(authController.isRouteProtected,authController.restrictTo('admin'),villaPackageBookingController.getAllVillaPackageBookings);
-
-router
   .route("/villa-package-bookings/:id")
   .get(villaPackageBookingController.getVillaPackageBooking)
-  .patch(authController.isRouteProtected,authController.restrictTo('admin'),villaPackageBookingController.updateVillaPackageBooking)
-  .delete(authController.isRouteProtected,authController.restrictTo('admin'),villaPackageBookingController.deleteVillaPackageBooking);
+  .patch(
+    authController.isRouteProtected,
+    authController.restrictTo("admin"),
+    villaPackageBookingController.updateVillaPackageBooking
+  )
+  .delete(
+    authController.isRouteProtected,
+    authController.restrictTo("admin"),
+    villaPackageBookingController.deleteVillaPackageBooking
+  );
 
 module.exports = router;
